401e53db123c142215dd158f78479b0c70d20106
Mill9510 <mill9510@stud.kea.dk>
2023-11-02 10:06:59 +0100

    tester

--- src/app/henry/page.jsx
+++ src/app/henry/page.jsx
@@ -1,3 +1,12 @@
+export async function generateMetadata() {
+  const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
+  const data = await res.json();
+  return {
+    title: data.name,
+    description: `Loves ${data.favouriteColor}`,
+  };
+}
+
 export default async function Henry() {
   const res = await fetch("https://nice-dogs.vercel.app/api/dogs?slug=henry");
   const data = await res.json();
@@ -9,6 +18,7 @@ export default async function Henry() {
       <h1>{name}</h1>
       <h2>Age {age}</h2>
       <h3>Favourite colour is {favouriteColor}</h3>
+      <p>hey</p>
     </main>
   );
 }
